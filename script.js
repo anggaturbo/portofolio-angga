14507a14d66632c533673e767568d45b18a5a73a
anggaturbo <anggaturbo87@gmail.com>
2025-07-13 23:17:36 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log('Portofolio Angga aktif');
+
+window.addEventListener("scroll", function () {
+  const scrollBtn = document.getElementById("scrollTopBtn");
+  scrollBtn.style.display = window.scrollY > 300 ? "block" : "none";
+});
+document.getElementById("scrollTopBtn")?.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
